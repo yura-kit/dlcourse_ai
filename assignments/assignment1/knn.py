4c8882cd768cda02d67bbba515d004e9e17a329c
Yuri Kitkevich <ykitkevich@exadel.com>
2019-03-07 00:26:27 +0300

    assigment1

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -55,7 +55,9 @@ class KNN:
         for i_test in range(num_test):
             for i_train in range(num_train):
                 # TODO: Fill dists[i_test][i_train]
-                pass
+                #dists[i_test][i_train] = np.sqrt(np.sum((X[i_test] - self.train_X[i_train])**2))
+                dists[i_test][i_train] = np.sum(np.abs(X[i_test] - self.train_X[i_train]), axis=0)
+        return dists
 
     def compute_distances_one_loop(self, X):
         '''
@@ -64,7 +66,13 @@ class KNN:
 
         Arguments:
         X, np array (num_test_samples, num_features) - samples to run
-        
+
+        The logic is simple — for each test vector I subtract it from the entire training matrix.
+        I use a trick called numpy addition broadcasting. As long as the dimensions match up,
+        numpy knows to do a row-wise subtraction if the element on the right is one-dimensional.
+        After this subtraction, I simply element-wise square and sum along the column dimension
+        to get a single row of the distance matrix for test vector i.
+
         Returns:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
@@ -72,16 +80,35 @@ class KNN:
         num_train = self.train_X.shape[0]
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
+        #print(dists.shape)
+        #print(self.train_X.shape)
         for i_test in range(num_test):
             # TODO: Fill the whole row of dists[i_test]
             # without additional loops
-            pass
+            # dists[i_test][i_train] = np.sum(np.abs(X[i_test] - self.train_X[i_train]), axis=0)
+            dists[i_test, :] = np.sum(np.abs(X[i_test, :] - self.train_X), axis=1)
+            #print(dists[i_test].shape)
+        return dists
 
     def compute_distances_no_loops(self, X):
         '''
         Computes distance from every sample of X to every training sample
         Fully vectorizes the calculations
+        NUMPY
+        https://habr.com/en/post/415373/
 
+        https://medium.com/dataholiks-distillery/l2-distance-matrix-vectorization-trick-26aa3247ac6c
+        -------------------------------------------------------------------------------------------
+        I need to vectorize more. How? After some Googling and old-fashioned pen and paper
+        I figured out better way. The “trick” is to expand the l2 distance formula.
+        For each vector x and y, the l2 distance between them can be expressed as:
+        (x - y)^2 = x^2 + y^2 - 2xy
+
+        To vectorize efficiently, we need to express this operation for ALL the vectors at once in numpy.
+        The first two terms are easy — just take the l2 norm of every row in the matrices X and X_train.
+        The last term can be expressed as a matrix multiply between X and transpose(X_train).
+        Numpy can do all of these things super efficiently.
+        -------------------------------------------------------------------------------------------
         Arguments:
         X, np array (num_test_samples, num_features) - samples to run
         
@@ -93,8 +120,17 @@ class KNN:
         num_test = X.shape[0]
         # Using float32 to to save memory - the default is float64
         dists = np.zeros((num_test, num_train), np.float32)
+        #print('required shape', dists.shape)
         # TODO: Implement computing all distances with no loops!
-        pass
+        #print('new shape of X', X[:,np.newaxis].shape )
+        #print('new shape of train_X', self.train_X[np.newaxis,:].shape)
+
+        dists = np.abs(X[:,np.newaxis] - self.train_X[np.newaxis,:])
+        dists = np.apply_along_axis(np.sum, 2, dists)
+        #print('X',X.shape)
+        #print('train X',self.train_X.shape)
+        #print('result',dists.shape)
+        return dists
 
     def predict_labels_binary(self, dists):
         '''
@@ -110,10 +146,24 @@ class KNN:
         '''
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
+        print('dist shape', dists.shape)
+        #print(pred.shape)
+        #print(pred)
+        print('y shape',self.train_y.shape)
+        print('result shape', pred.shape)
+        print('k=',self.k)
+        print(pred)
         for i in range(num_test):
             # TODO: Implement choosing best class based on k
             # nearest training samples
-            pass
+            #print(dists[i])
+            #print(np.argsort(dists[i, :])[:self.k])
+            closest_y = self.train_y[np.argsort(dists[i, :])[:self.k]]# sort by value then get indexes by slicing till k (first k in each row)
+
+            u, indices = np.unique(closest_y, return_inverse=True)
+            #print('U',u, indices)
+            #print(u[np.argmax(np.bincount(indices))])
+            pred[i] = u[np.argmax(np.bincount(indices))]
         return pred
 
     def predict_labels_multiclass(self, dists):
@@ -134,5 +184,11 @@ class KNN:
         for i in range(num_test):
             # TODO: Implement choosing best class based on k
             # nearest training samples
-            pass
+            closest_y = self.train_y[np.argsort(dists[i, :])[:self.k]]
+            # sort by value then get indexes by slicing till k (first k in each row)
+            print((closest_y))
+            u, indices = np.unique(closest_y, return_inverse=True)
+            print('U',u, indices)
+            print(u[np.argmax(np.bincount(indices))])
+            pred[i] = u[np.argmax(np.bincount(indices))]
         return pred
--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -1,3 +1,10 @@
+import numpy as np
+from sklearn.metrics import precision_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
+
 def binary_classification_metrics(prediction, ground_truth):
     '''
     Computes metrics for binary classification
@@ -14,11 +21,27 @@ def binary_classification_metrics(prediction, ground_truth):
     accuracy = 0
     f1 = 0
 
+    print('predictions', prediction)
+    print('      truth', ground_truth)
+    print('         cw',np.bitwise_xor(prediction, ground_truth))
+    tp = np.sum(np.bitwise_and(prediction, ground_truth))
+    print('TP=', tp)
+    fp = np.sum(np.bitwise_xor(prediction, ground_truth))
+
+    print('FP=', fp)
+    precision = tp/(tp + fp)
+    print('precision=', precision)
+    print('preciiosnn_real', precision_score(ground_truth, prediction))
     # TODO: implement metrics!
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-    
+
+    precision = precision_score(ground_truth, prediction)
+    recall = recall_score(ground_truth, prediction)
+    f1 = f1_score(ground_truth, prediction)
+    accuracy = accuracy_score(ground_truth, prediction)
+
     return accuracy, precision, recall, f1
 
 
@@ -34,4 +57,5 @@ def multiclass_accuracy(prediction, ground_truth):
     accuracy - ratio of accurate predictions to total samples
     '''
     # TODO: Implement computing accuracy
-    return 0
+
+    return accuracy_score(ground_truth, prediction)
